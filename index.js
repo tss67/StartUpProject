ff485708768aa2f90b6560d95f2d99beb1b4ccf4
Taylor Smith <tss67@byu.edu>
2023-12-14 20:48:02 -0700

    Finished backend service endpoints, called service endpoints from frontend

--- index.js
+++ index.js
@@ -10,23 +10,20 @@ app.use(express.json());
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
 
-
-/*
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-// GetScores
-apiRouter.get('/scores', (_req, res) => {
-  res.send(scores);
+// GetCounts
+apiRouter.get('/counts', (_req, res) => {
+  res.send({count: counts});
 });
 
-// SubmitScore
-apiRouter.post('/score', (req, res) => {
-  scores = updateScores(req.body, scores);
-  res.send(scores);
+// PostCount
+apiRouter.post('/count', (req, res) => {
+  counts = req.body.count;
+  res.send({count: counts});
 });
-*/
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
@@ -37,28 +34,4 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-/*
-// updateScores considers a new score for inclusion in the high scores.
-// The high scores are saved in memory and disappear whenever the service is restarted.
-let scores = [];
-function updateScores(newScore, scores) {
-  let found = false;
-  for (const [i, prevScore] of scores.entries()) {
-    if (newScore.score > prevScore.score) {
-      scores.splice(i, 0, newScore);
-      found = true;
-      break;
-    }
-  }
-
-  if (!found) {
-    scores.push(newScore);
-  }
-
-  if (scores.length > 10) {
-    scores.length = 10;
-  }
-
-  return scores;
-}
-*/
+let counts = 0;
--- public/edit.js
+++ public/edit.js
@@ -1,38 +1,68 @@
-function initializeCounts() {
+async function getCurrentCounts() {
+    let countText = "";
+
     //Get current counts
-    let pageCount = localStorage.getItem("page_count");
-    let choiceCount = localStorage.getItem("choice_count");
+    try {
+        // Get the latest high scores from the service
+        const response = await fetch('/api/counts');
+        const responseJson = await response.json();
+
+        // Save the scores in case we go offline in the future
+        countText =  JSON.stringify(responseJson.count);
+        localStorage.setItem('page_and_choice_count', countText);
+    } catch {
+        // If there was an error then just use the last saved scores
+        countText = localStorage.getItem('page_and_choice_count');
+    }
+
+    return countText;
+}
+
+async function setCount(newCount) {
+    try {
+        const response = await fetch('/api/count', {
+          method: 'POST',
+          headers: {'content-type': 'application/json'},
+          body: JSON.stringify({count: newCount}),
+        });
+  
+        // Store what the service gave us as the high scores
+        const counts = await response.json();
+        localStorage.setItem("page_and_choice_count", JSON.stringify(counts.count));
+    } catch {
+        // If there was an error then just track scores locally
+        localStorage.setItem("page_and_choice_count", newCount);
+    }
+}
+
+async function initializeCounts() {
+
+    let count = await getCurrentCounts();
 
     //If nothing exists yet, initialize to a random value for now
-    if(pageCount === "" || !pageCount) {
-        pageCount = "0";
-        localStorage.setItem("page_count", pageCount);
-    }
-    if(choiceCount === "" || !choiceCount) {
-        choiceCount = "0";
-        localStorage.setItem("choice_count", choiceCount);
+    if(count === "" || !count) {
+        count = 0
+        await setCount(count)
     }
 
     //Update visual elements
     const pageCountEl = document.querySelector('#pageCount');
     const choiceCountEl = document.querySelector('#choiceCount');
 
-    pageCountEl.innerHTML =  "Total Pages So Far: " + pageCount
-    choiceCountEl.innerHTML =  "Total Choices So Far: " + choiceCount
+    pageCountEl.innerHTML =  "Total Pages So Far: " + count;
+    choiceCountEl.innerHTML =  "Total Choices So Far: " + count;
 }
 
-function increaseCount() {
+async function increaseCount() {
     //Get current counts
-    let pageCount = parseInt(localStorage.getItem("page_count"));
-    let choiceCount = parseInt(localStorage.getItem("choice_count"));
+    let newCount = parseInt(localStorage.getItem("page_and_choice_count"));
 
     //Increase by 1
-    pageCount += 1;
-    choiceCount += 1;
+    newCount += 1;
 
-    localStorage.setItem("page_count", pageCount);
-    localStorage.setItem("choice_count", choiceCount);
+    await setCount(newCount);
 }
+
 
 //Third Party Endpoint
 function displayJoke(data) {
